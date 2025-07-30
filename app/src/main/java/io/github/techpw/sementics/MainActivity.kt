75dfa425b6ede55aaf9318e4a00c9715af948832
Mullick <farhazul.mullick@pw.live>
2025-07-30 21:50:19 +0530

    Updated testing code & CHANGELOGS.md

--- app/src/main/java/io/github/techpw/sementics/MainActivity.kt
+++ app/src/main/java/io/github/techpw/sementics/MainActivity.kt
@@ -2,28 +2,26 @@ package io.github.techpw.sementics
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import live.pw.compose.semantics.R
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+        //io.github.techpw.semantics:id/auto_login_MainActivity_Column_Text_1340562411
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val composeView = findViewById<ComposeView>(R.id.compose_view)
@@ -39,41 +37,73 @@ class MainActivity : ComponentActivity() {
 fun App() {
     Column(modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "hello world 1")
-        MyText(text = "hello world 2")
-        Text(text = "hello world 4", modifier = Modifier.padding(16.dp))
+//        Text(text = "hello world 0")
+//        Text(text = "hello world 1") //io.github.techpw.semantics:id/auto_login_MainActivity_Column_Text_498198592
+//
+//        //io.github.techpw.semantics:id/auto_login_MainActivity_Column_MyText_-1671319640
+//        MyText(text = "hello world 2")
+//
+//        Text(text = "Hello world 3", style = TextStyle.Default)
+//
+//        // io.github.techpw.semantics:id/auto_login_MainActivity_Column_Text_-1416452045
+//        Text(text = "hello world 4", modifier = Modifier.padding(16.dp)) // io.github.techpw.semantics:id/auto_login_MainActivity_Column_Text_-1416452045
+//
 
-        Button(modifier = Modifier , onClick = {
+        // io.github.techpw.semantics:id/auto_login_MainActivity_Column_Row_-1282579546
+//        Row(modifier = Modifier
+//            .fillMaxWidth()
+//            .background(color = Color.Green)
+//            .height(50.dp)
+//        ) {}
+//
+//        // io.github.techpw.semantics:id/auto_login_MainActivity_Column_Text_1841024471
+//        Text(text = stringResource(R.string.testing_text))
+//
+//        //io.github.techpw.semantics:id/auto_login_MainActivity_Column_Text_-507501049
+        Text(text = stringResource(R.string.hello_world_6))
+
+        Button(modifier = Modifier , onClick = { // io.github.techpw.semantics:id/auto_login_MainActivity_Text_-539451011
             println("button clicked")
         }){
             Text(text = "Button B", modifier = Modifier)
         }
+        //io.github.techpw.semantics:id/auto_login_MainActivity_Column_HorizontalPager_416281447
+        HorizontalPager(state = rememberPagerState { 0 }, modifier = Modifier.size(20.dp)) { }
+        //io.github.techpw.semantics:id/auto_login_MainActivity_Column_HorizontalPager_416281447
+        HorizontalPager(state = rememberPagerState { 0 }, modifier = Modifier.size(21.dp)) { }
 
-        Button(modifier = Modifier , onClick = {
-            println("button clicked")
-        }){
-            Text(text = "Button C", modifier = Modifier)
-        }
+//        Button(modifier = Modifier , onClick = {
+//            println("button clicked")
+//        }){
+//            Text(text = "Button C", modifier = Modifier)
+//        }
 
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .background(color = Color.Green)
-            .height(56.dp)
-        ) {}
+        //Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .background(color = Color.Green)
-            .height(56.dp)
-        ) {}
+        // io.github.techpw.semantics:id/auto_login_MainActivity_Column_Row_-382487263
+        // io.github.techpw.semantics:id/auto_login_MainActivity_Column_Row_-1337241181
+        // io.github.techpw.semantics:id/auto_login_MainActivity_Column_Row_1653461923
+        // io.github.techpw.semantics:id/auto_login_MainActivity_Column_Row_29622250
+        // io.github.techpw.semantics:id/auto_login_MainActivity_Column_Row_-570331632
+//        Row(modifier = Modifier
+//            .fillMaxWidth()
+//            .background(color = Color.Green)
+//            .height(56.dp)
+//        ) {}
 
-        Widget()
+        // Spacer(modifier = Modifier.height(16.dp))
+//
+//        Row(modifier = Modifier
+//            .fillMaxWidth()
+//            .background(color = Color.Green)
+//            .height(56.dp)
+//        ) {}
+
+        //Widget()
     }
 }
 
-@Composable
-fun MyText(modifier: Modifier = Modifier.padding(2.dp), text: String) {
-    Text(modifier = modifier, text = text)
-}
+//@Composable
+//fun MyText(modifier: Modifier = Modifier.padding(2.dp), text: String) {
+//    Text(modifier = modifier, text = text)
+//}
--- app/src/main/java/io/github/techpw/sementics/TestingFile1.kt
+++ app/src/main/java/io/github/techpw/sementics/TestingFile1.kt
@@ -1,97 +1,97 @@
-package io.github.techpw.sementics
-
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.testTag
-import androidx.compose.ui.semantics.testTagsAsResourceId
-
-
-@OptIn(ExperimentalComposeUiApi::class)
-@Composable
-fun App2() {
-    Column(modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally) {
-
-        Text(text = "hello world 1", style = MaterialTheme.typography.titleLarge)
-        // Semantic modifier passed to ComposableA.
-        A(
-            text = "A with semantics",
-            modifier = Modifier.semantics(false) {
-                testTag = "tag-a"
-                testTagsAsResourceId = true
-            }
-        )
-
-
-        // Calling ComposableAB directly with semantics modifier.
-        B(
-            text = "B with semantics",
-            modifier = Modifier
-        )
-
-        C(
-            text = "C with semantics",
-            modifier = Modifier.semantics(true) {
-                testTag = "tag-c"
-                testTagsAsResourceId = true
-            }
-        )
-
-        D(
-            text = "D with semantics",
-            modifier = Modifier.semantics(true) {
-                testTag = "tag-d"
-                testTagsAsResourceId = true
-            }
-        )
-    }
-}
-
-@OptIn(ExperimentalComposeUiApi::class)
-@Composable
-fun A(modifier: Modifier = Modifier, text: String) {
-    // B is accepting parent modifier of a.
-    B(
-        modifier = modifier.semantics(true) {
-            testTag = "tag-b"
-            testTagsAsResourceId = true
-        },
-        text = text
-    )
-}
-
-@OptIn(ExperimentalComposeUiApi::class)
-@Composable
-fun B(modifier: Modifier = Modifier, text: String) {
-    Column (modifier = modifier){
-        Text(modifier = modifier, text = text, style = MaterialTheme.typography.titleLarge)
-    }
-}
-
-@Composable
-fun C(modifier: Modifier = Modifier, text: String) {
-    Text(modifier = modifier, text = text, style = MaterialTheme.typography.titleLarge)
-}
-
-@OptIn(ExperimentalComposeUiApi::class)
-@Composable
-fun D(modifier: Modifier, text: String) {
-    Button(
-        modifier = modifier
-            .semantics(true) {
-                testTag = "tag-button"
-                testTagsAsResourceId = true
-            },
-        onClick = { /* Do something */ }
-    ) {
-        Text(text)
-    }
-}
+//package io.github.techpw.sementics
+//
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.material3.Button
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.ExperimentalComposeUiApi
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.semantics.semantics
+//import androidx.compose.ui.semantics.testTag
+//import androidx.compose.ui.semantics.testTagsAsResourceId
+//
+//
+//@OptIn(ExperimentalComposeUiApi::class)
+//@Composable
+//fun App2() {
+//    Column(modifier = Modifier.fillMaxSize(),
+//        horizontalAlignment = Alignment.CenterHorizontally) {
+//
+//        Text(text = "hello world 1", style = MaterialTheme.typography.titleLarge)
+//        // Semantic modifier passed to ComposableA.
+//        A(
+//            text = "A with semantics",
+//            modifier = Modifier.semantics(false) {
+//                testTag = "tag-a"
+//                testTagsAsResourceId = true
+//            }
+//        )
+//
+//
+//        // Calling ComposableAB directly with semantics modifier.
+//        B(
+//            text = "B with semantics",
+//            modifier = Modifier
+//        )
+//
+//        C(
+//            text = "C with semantics",
+//            modifier = Modifier.semantics(true) {
+//                testTag = "tag-c"
+//                testTagsAsResourceId = true
+//            }
+//        )
+//
+//        D(
+//            text = "D with semantics",
+//            modifier = Modifier.semantics(true) {
+//                testTag = "tag-d"
+//                testTagsAsResourceId = true
+//            }
+//        )
+//    }
+//}
+//
+//@OptIn(ExperimentalComposeUiApi::class)
+//@Composable
+//fun A(modifier: Modifier = Modifier, text: String) {
+//    // B is accepting parent modifier of a.
+//    B(
+//        modifier = modifier.semantics(true) {
+//            testTag = "tag-b"
+//            testTagsAsResourceId = true
+//        },
+//        text = text
+//    )
+//}
+//
+//@OptIn(ExperimentalComposeUiApi::class)
+//@Composable
+//fun B(modifier: Modifier = Modifier, text: String) {
+//    Column (modifier = modifier){
+//        Text(modifier = modifier, text = text, style = MaterialTheme.typography.titleLarge)
+//    }
+//}
+//
+//@Composable
+//fun C(modifier: Modifier = Modifier, text: String) {
+//    Text(modifier = modifier, text = text, style = MaterialTheme.typography.titleLarge)
+//}
+//
+//@OptIn(ExperimentalComposeUiApi::class)
+//@Composable
+//fun D(modifier: Modifier, text: String) {
+//    Button(
+//        modifier = modifier
+//            .semantics(true) {
+//                testTag = "tag-button"
+//                testTagsAsResourceId = true
+//            },
+//        onClick = { /* Do something */ }
+//    ) {
+//        Text(text)
+//    }
+//}
